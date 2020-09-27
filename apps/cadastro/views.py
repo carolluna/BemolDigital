8ee64b18f6e7a36651851927bb7d5f5a72ca8552
Anna Carolina de Luna <ac.luna95@gmail.com>
2020-09-27 19:58:42 -0400

    Create a view AllAddress and Register Client

--- apps/cadastro/views.py
+++ apps/cadastro/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.views import APIView
 
-# Create your views here.
+from .tasks import Address
+
+class AllAddress(APIView):
+
+    def get(self, request):
+
+        if 'cep' in request.query_params.keys():
+            cep = request.query_params['cep']
+            try:
+                address = Address(cep)
+                address.validate_cep()
+                all_address = address.get_address_info()
+                return HttpResponse(all_address)
+            except ValueError as ex:
+                return HttpResponse(ex)
+        else:
+            return HttpResponse('No CEP data')
+
+class RegisterClient(APIView):
+    pass
+

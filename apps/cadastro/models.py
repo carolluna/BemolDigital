5bdc97a27bc373c2d5596698510853d9326252d9
Anna Carolina de Luna <ac.luna95@gmail.com>
2020-09-26 20:29:53 -0400

    Modeling User database

--- apps/cadastro/models.py
+++ apps/cadastro/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField("name", max_length=50)
+    birth = models.DateField("birth", auto_now=False, auto_now_add=False)
+    cep = models.CharField("cep", max_length=8)
+    street = models.CharField("street", max_length=100)
+    home_number = models.CharField("home_number", max_length=10)
+    neighborhood = models.CharField("neighborhood", max_length=50)
+    city = models.CharField("city", max_length=100)
+    country = models.CharField("country", max_length=100)
+    phone = models.CharField("phone_number", max_length=50)
+    email = models.EmailField("email", max_length=254)
+

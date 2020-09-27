895ddca57863e8aed6cf6a9800807f05825369b7
Anna Carolina de Luna <ac.luna95@gmail.com>
2020-09-27 19:57:25 -0400

    Change lenght of name, phone on models

--- apps/cadastro/models.py
+++ apps/cadastro/models.py
@@ -1,16 +1,15 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField("name", max_length=50)
+    name = models.CharField("name", max_length=100)
     birth = models.DateField("birth", auto_now=False, auto_now_add=False)
     cep = models.CharField("cep", max_length=8)
     street = models.CharField("street", max_length=100)
     home_number = models.CharField("home_number", max_length=10)
-    neighborhood = models.CharField("neighborhood", max_length=50)
+    neighborhood = models.CharField("neighborhood", max_length=100)
     city = models.CharField("city", max_length=100)
     uf = models.CharField("uf", max_length=2)
-    phone = models.CharField("phone_number", max_length=50)
+    phone = models.CharField("phone_number", max_length=11)
     email = models.EmailField("email", max_length=254)
-    cpf = models.CharField("cpf", max_length=11)
     password = models.CharField("password", max_length=50)
 

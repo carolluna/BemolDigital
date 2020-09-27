458edbf077b1f1883cf1417fecef19f1c475bb8c
Anna Carolina de Luna <ac.luna95@gmail.com>
2020-09-27 19:58:15 -0400

    Add urls address and register

--- apps/cadastro/urls.py
+++ apps/cadastro/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from . import views
 
 urlpatterns = [
+    path('address/', views.AllAddress.as_view()),
+    path('register/' views.RegisterClient.as_view())
 
 ]
